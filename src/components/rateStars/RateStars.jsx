a9901899c7c06c3d6e442d6571ab47320a916c5c
martinacev <103221910+martinacev@users.noreply.github.com>
2024-06-13 14:12:17 +0200

    stars-fixing-click

--- src/components/rateStars/RateStars.jsx
+++ src/components/rateStars/RateStars.jsx
@@ -2,27 +2,32 @@ import { useState } from "react";
 import classes from "./RateStars.module.css";
 
 const RateStars = () => {
-  const [clicked, setClicked] = useState(false);
+  const [clickedStars, setClickedStars] = useState(new Array(5).fill(false));
 
-  const handleClick = () => {
-    setClicked(!clicked);
+  const handleClick = (index) => {
+    const newClickedStars = [...clickedStars];
+
+    newClickedStars[index] = !newClickedStars[index];
+
+    for (let i = 0; i <= index; i++) {
+      newClickedStars[i] = true;
+    }
+    setClickedStars(newClickedStars);
   };
 
-  const stars = [1, 2, 3, 4, 5]; 
+  const stars = [1, 2, 3, 4, 5];
 
   return (
     <div className={classes.container}>
-      {stars.map((starId) => (
+      {stars.map((starId, index) => (
         <svg
-          key={starId} 
-          fill="none"
-          onClick={handleClick}
+          key={starId}
+          fill={clickedStars[index] ? "gold" : "grey"}
+          onClick={() => handleClick(index)}
           style={{ cursor: "pointer" }}
+          viewBox="0 0 60 45"
         >
-          <path
-            d="M33.6923 32.0283L41.8146 29.5849L45.2081 42L33.6923 32.0283ZM52.3846 15.9811H38.0872L33.6923 0L29.2974 15.9811H15L26.5714 25.8868L22.1765 41.8679L33.7479 31.9623L40.8688 25.8868L52.3846 15.9811Z"
-            fill={clicked ? "gold" : "grey"}
-          />
+          <path d="M33.6923 32.0283L41.8146 29.5849L45.2081 42L33.6923 32.0283ZM52.3846 15.9811H38.0872L33.6923 0L29.2974 15.9811H15L26.5714 25.8868L22.1765 41.8679L33.7479 31.9623L40.8688 25.8868L52.3846 15.9811Z" />
         </svg>
       ))}
     </div>
